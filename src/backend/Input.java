bf335367b509a2cf909a67ec337d43560bf1e7b2
Zach Huxford <zachbwh@gmail.com>
2017-04-21 15:52:33 +1200

    created method to intialise the Emoji hashtable and parse data from the emoji file given by the Unicode Consortium into the Emoji Objects.

--- src/backend/Input.java
+++ src/backend/Input.java
@@ -3,9 +3,12 @@ package backend;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Set;
 
 public class Input {
 	public ArrayList<String> emojiList = new ArrayList<String>();
+	public Hashtable<String, Emoji> emojis = new Hashtable<String, Emoji>();
 	
 	public void initialiseEmojiList(String filepath) throws Exception {
 		FileReader fr = new FileReader(filepath);
@@ -23,6 +26,46 @@ public class Input {
 	public void printEmojiList() {
 		for (String s: this.emojiList) {
 			System.out.println(s);
+		}
+	}
+	public void buildEmojiHashtable() {
+		for(String s: this.emojiList) {
+			String[] lineArray = s.split("\\s+");
+			int counter = 0;
+			ArrayList<String> unicodeCode = new ArrayList<String>();
+			ArrayList<String> keywords = new ArrayList<String>();
+			String cldrShortName = "";
+			String emojiChar = "";
+			for (int i = 0; i < lineArray.length; i++) {
+				if (lineArray[i].equals(";") || lineArray[i].equals("#")) {
+					counter++;
+				} else if (counter == 0) {
+					unicodeCode.add(lineArray[i]);
+				} else if (counter == 2) {
+					emojiChar = emojiChar + lineArray[i];
+					counter++;
+				} else if (counter > 2) {
+					cldrShortName = cldrShortName + " " + lineArray[i];
+					keywords.add(lineArray[i]);
+				}
+
+				// System.out.print(lineArray[i] + " ");
+			}
+			// System.out.print("\n");
+			String[] unicodeCodeArray = new String[unicodeCode.size()];
+			unicodeCodeArray = unicodeCode.toArray(unicodeCodeArray);
+			String[] keywordsArray = new String[keywords.size()];
+			keywordsArray = keywords.toArray(keywordsArray);
+			Emoji e = new Emoji(cldrShortName, unicodeCodeArray, keywordsArray, emojiChar);
+			this.emojis.put(cldrShortName, e);
+		}
+		Set<String> keys = this.emojis.keySet();
+		for (String s: keys) {
+			// System.out.println(s + ":");
+			emojis.get(s).printEmojiName();
+			emojis.get(s).printEmojiChar();
+			emojis.get(s).printEmojiCode();
+			System.out.println("");
 		}
 	}
 }

e336a085f1e8b659a133d6be6ce0011cef54c081
Zach Huxford <zachbwh@gmail.com>
2017-04-21 15:50:33 +1200

    added _emojiChar field and more methods for querying a specific Emoji object.

--- src/backend/Emoji.java
+++ src/backend/Emoji.java
@@ -2,14 +2,16 @@ package backend;
 
 public class Emoji {
 	private final String _cldrShortName;
+	private final String _emojiChar;
 	private final String[] _unicodeCode;
 	private final String[] _keywords;
 	private int _totalUses;
 	
-	public Emoji(String cldrShortName, String[] unicodeCode, String[] keywords) {
+	public Emoji(String cldrShortName, String[] unicodeCode, String[] keywords, String emojiChar) {
 		this._cldrShortName = cldrShortName;
 		this._unicodeCode = unicodeCode;
 		this._keywords = keywords;
+		this._emojiChar = emojiChar;
 	}
 	
 	public void assignTotalUses(int n) {
@@ -21,5 +23,29 @@ public class Emoji {
 	
 	public void incrementTotalUses() {
 		this._totalUses++;
+	}
+	
+	public void printEmojiName() {
+		System.out.print(this._cldrShortName);
+	}
+	
+	public void printEmojiCode() {
+		for (int i = 0; i < _unicodeCode.length; i++) {
+			System.out.print("\\u" + this._unicodeCode[i] + " ");
+		}
+		// System.out.print("\n");
+	}
+	
+	public void printEmojiChar() {
+		System.out.print(_emojiChar);
+	}
+	
+	public boolean keywordsContainWord(String word) {
+		for (int i = 0; i < this._keywords.length; i++) {
+			if (_keywords[i].startsWith(word)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }

06a7d8821c1beca5188a662c24d62fabc01bfcdc
Zach Huxford <zachbwh@gmail.com>
2017-04-21 15:48:42 +1200

    created Seacrh class with keyword search method

---
+++ src/backend/Search.java
@@ -0,0 +1,15 @@
+package backend;
+
+import java.util.Hashtable;
+import java.util.Set;
+
+public class Search {
+	public void keywordSearch(Hashtable<String, Emoji> emojiList, String search) {
+		Set<String> keys = emojiList.keySet();
+		for (String s: keys) {
+			if (emojiList.get(s).keywordsContainWord(search)) {
+				
+			}
+		}
+	}
+}
